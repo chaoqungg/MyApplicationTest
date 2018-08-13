e0b199837993e4b61782268a9ee283209a5c0f39
zcq <chaoqungg@sina.com>
2018-08-13 14:59:14 +0800

    添加了oncreate()方法

--- app/src/main/java/com/example/zcq/myapplicationtest/NewAdd.java
+++ app/src/main/java/com/example/zcq/myapplicationtest/NewAdd.java
@@ -1,6 +1,13 @@
 package com.example.zcq.myapplicationtest;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 public class NewAdd extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
