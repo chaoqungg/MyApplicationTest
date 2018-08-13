406fa274459442bc090dd0aa83a22bfe1ff89e7a
zcq <chaoqungg@sina.com>
2018-08-13 15:23:27 +0800

    a new branch

--- app/src/main/java/com/example/zcq/myapplicationtest/NewAdd.java
+++ app/src/main/java/com/example/zcq/myapplicationtest/NewAdd.java
@@ -1,6 +1,7 @@
 package com.example.zcq.myapplicationtest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
@@ -9,5 +10,11 @@ public class NewAdd extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
